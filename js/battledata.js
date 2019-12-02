b5c8ce1be8390e3ab801cc66a79017f23cdc692c
Poilerwags <giestheuws@gmail.com>
2019-12-02 10:35:41 +0100

    sprites attempt 2

--- js/battledata.js
+++ js/battledata.js
@@ -221,7 +221,7 @@ pokeballs=null;this.
 resourcePrefix=function(){var _window$document,_window$document$loca;
 var prefix='';
 if(((_window$document=window.document)==null?void 0:(_window$document$loca=_window$document.location)==null?void 0:_window$document$loca.protocol)!=='http:')prefix='https:';
-return prefix+"//raw.githubusercontent.com/Poilerwags/Sprites/master/play.pokemonshowdown.com/";
+return prefix+"raw.githubusercontent.com/Poilerwags/Sprites/master/play.pokemonshowdown.com";
 }();this.
 
 fxPrefix=function(){var _window$document2,_window$document2$loc;
