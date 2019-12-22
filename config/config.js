42a97e3d7f6805ba6ad65d0e4d46ba9f97a6b83a
Poilerwags <giestheuws@gmail.com>
2019-12-22 17:05:12 +0100

    format+melo

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var Config = Config || {};
 
-/* version */ Config.version = "0.11.2 (cca85725)";
+/* version */ Config.version = "0.11.2 (86c39b22)";
 
 Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
 
